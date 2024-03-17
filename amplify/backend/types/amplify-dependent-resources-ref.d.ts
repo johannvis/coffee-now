3fb642546ae0274d6731fe3371458cdf889a5323
johannvis <johannvis@gmail.com>
2024-03-17 05:36:05 +0000

    Your commit message

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,43 +1,13 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "api17fef00a": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    },
     "order": {
       "ApiId": "string",
       "ApiName": "string",
       "RootUrl": "string"
     }
   },
-  "auth": {
-    "coffeenow752e3ad5": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
   "function": {
-    "CreateNewOrder": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "coffeenow2cd1dcad": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    },
-    "coffeenowb3f21ce5": {
+    "AddOrder": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
